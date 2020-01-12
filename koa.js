0565cceb89ff72f9b5f60803b32f2663e2b60f79
ppambler <allinlbj@gmail.com>
2020-01-12 15:19:30 +0800

    next()的返回值

--- koa.js
+++ koa.js
@@ -12,6 +12,21 @@ router.get("/user", ctx => {
   ctx.body = "用户首页";
 });
 
+app.use((ctx, next) => {
+  console.log(1);
+  next().then(res => {
+    console.log(res);
+  });
+  console.log(2);
+});
+
+app.use((ctx, next) => {
+  console.log(3);
+  next();
+  console.log(4);
+  return "我是你要的数据";
+});
+
 // 定义了路由，就要使用它
 app.use(router.routes());
 
